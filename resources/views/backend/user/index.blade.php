77c3e87a9a8325420270a0da73420644c20e6257
seoFermer <sv.seofermer@gmail.com>
2022-10-09 21:36:40 +0300

    test step 2

--- resources/views/backend/user/index.blade.php
+++ resources/views/backend/user/index.blade.php
@@ -8,6 +8,7 @@
                 <div class="col-sm-6">
                     <h1 class="m-0">{{ trans('translate.User') }}</h1>
                     <h2>Test</h2>
+                    <h2>Test + 2</h2>
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
